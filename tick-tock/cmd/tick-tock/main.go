5841d44e22a026983329dd8e8f94074f755ae2b1
lyydsheep <2230561977@qq.com>
2025-07-04 19:30:57 +0800

    project setup: 搭建 main 函数

--- tick-tock/cmd/tick-tock/main.go
+++ tick-tock/cmd/tick-tock/main.go
@@ -2,26 +2,54 @@ package main
 
 import (
 	"github.com/go-kratos/kratos/v2"
-	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-kratos/kratos/v2/transport/http"
-	"time"
+	"os"
+	"tick-tock/configs"
+	"tick-tock/pkg/log"
 )
 
+// go build -ldflags "-X main.Version=x.y.z"
+var (
+	// Name is the name of the compiled software.
+	Name string
+	// Version is the version of the compiled software.
+	Version string
+	// flagconf is the config flag.
+	flagconf string
+
+	id, _ = os.Hostname()
+)
+
+func newApp(gs *grpc.Server, hs *http.Server) *kratos.App {
+	return kratos.New(
+		kratos.ID(id),
+		kratos.Name(Name),
+		kratos.Version(Version),
+		kratos.Metadata(map[string]string{}),
+		kratos.Server(
+			gs,
+			hs,
+		),
+	)
+}
+
 func main() {
-	httpSrv := http.NewServer(http.Address(":8080"))
-	app := kratos.New(kratos.Name("tick-tock"), kratos.Server(httpSrv))
+	server, data := configs.NewConfig()
 
-	go func() {
-		ticker := time.NewTicker(time.Second)
-		for {
-			select {
-			case <-ticker.C:
-				log.Info("tick")
-			}
-		}
-	}()
+	app, cleanup, err := wireApp(server, data)
+	if err != nil {
+		panic(err)
+	}
+	defer cleanup()
 
+	log.Info(nil, "new app successfully",
+		"service.id", id,
+		"service.name", Name,
+		"service.version", Version)
+
+	// start and wait for stop signal
 	if err := app.Run(); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
--- tick-tock/cmd/tick-tock/wire.go
+++ tick-tock/cmd/tick-tock/wire.go
@@ -13,11 +13,10 @@ import (
 	"tick-tock/internal/service"
 
 	"github.com/go-kratos/kratos/v2"
-	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
 )
 
 // wireApp init kratos application.
-func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+func wireApp(*conf.Server, *conf.Data) (*kratos.App, func(), error) {
 	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
 }
